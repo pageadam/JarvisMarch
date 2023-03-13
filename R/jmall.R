286ed1b16f302d781aff957bd75070d25f62605b
Adam Page <126669635+pageadam@users.noreply.github.com>
2023-03-13 18:14:08 +0000

    Add files via upload

---
+++ R/jmall.R
@@ -0,0 +1,27 @@
+jarvis_march_all<-function(input){
+  if(class(input) == "list"){
+  
+	jarvis_march(input)
+	
+  }else if(class(input)=="numeric"){
+  
+  	list_of_points <- list()
+  	for(i in 1:(length(input)/2)){
+  	  list_of_points[i] <- list(c(input[2*i-1],input[2*i]))
+  	}
+  	
+  	jarvis_march(list_of_points)
+	
+  }else if(class(input)=="data.frame"){
+  
+    list_of_points<-list()
+    for(i in 1:nrow(input)){
+      list_of_points[i] <- list(c(input[i,1],input[i,2]))
+    }
+	
+    jarvis_march(list_of_points)
+	
+  }else{
+	print(paste("Input was of the type", class(input),". Please input in the form of a list, vector or data.frame") )
+  }
+}
