ec08080fbef80ea30e48f31858fa33500cdae7bf
Adam Page <126669635+pageadam@users.noreply.github.com>
2023-03-20 18:14:51 +0000

    Add files via upload

---
+++ R/inhull.R
@@ -0,0 +1,60 @@
+in_hull<-function(Original_Set,Test_Point,print=TRUE){
+  if(class(Test_Point) == "list"){
+    for (i in 1:length(Test_Point)) {
+      if(length(Test_Point[[i]]) != 2){
+        stop("Not all test points have length 2")
+      }
+    }
+    list_of_points<-Test_Point
+  }else if(class(Test_Point)=="numeric"){
+    
+    if(length(Test_Point)%%2 != 0){
+      stop("Not all points have length 2")
+    }
+    
+    list_of_points <- list()
+    for(i in 1:(length(Test_Point)/2)){
+      list_of_points[i] <- list(c(Test_Point[2*i-1],Test_Point[2*i]))
+    }
+  }else if(class(Test_Point)=="data.frame"){
+    
+    list_of_points<-list()
+    for(i in 1:nrow(Test_Point)){
+      list_of_points[i] <- list(c(Test_Point[i,1],Test_Point[i,2]))
+    }
+  }else{
+    stop("Test input not in the class of a list, numeric vector or data frame")
+  }
+  
+  Hull<-jarvis_march_all(Original_Set)
+  count<-0
+  for( i in 1:length(Hull)){
+    for(j in 1:length(list_of_points)){
+      if((Hull[[i]][1]==list_of_points[[j]][1]) & ((Hull[[i]][2]==list_of_points[[j]][2]))){
+        if(print==TRUE){
+          print(paste("Point","(",list_of_points[[j]][1],",",list_of_points[[j]][2],")","is in the hull"))
+        }
+        count<-count+1
+      }
+    }
+  }
+  
+  if(length(list_of_points)==count){
+    if(print==TRUE){
+      print("All tested points are in the Convex Hull")
+    }
+    return(TRUE)
+  }else if((length(list_of_points)>count) & (count>0)){
+    if(print==TRUE){
+      print("Some tested points are in the Convex Hull")
+    }
+    return(FALSE)
+  }else if(count == 0){
+    if(print==TRUE){
+      print("No tested points are in the Convex Hull")
+    }
+    return(FALSE)
+  }else{
+    print("uhhhhh somethings gone wrong")
+  }
+}
---
+++ R/jmall.R
@@ -0,0 +1,105 @@
+jarvis_march_all<-function(input,plot=FALSE){
+  if(class(input) == "list"){
+    
+    for (i in 1:length(input)) {
+      if(length(input[[i]]) != 2){
+        stop("Not all points have length 2")
+      }
+    }
+    output<-jarvis_march(input)
+    if(plot==TRUE){
+      require(rlang)
+      require(ggplot2)
+      X=c()
+      Y=c()
+      for (i in 1:length(input)) {
+        X[i] = input[[i]][1]
+        Y[i] = input[[i]][2]
+      }
+      df=data.frame(X, Y)
+      p = ggplot(df,aes(x=X,y=Y)) + geom_point()
+      hullX=c()
+      hullY=c()
+      for (i in 1:length(output)) {
+        hullX[i] = output[[i]][1]
+        hullY[i] = output[[i]][2]
+      }
+      hullX[length(output)+1] = output[[length(output)]][3] ; hullY[length(output)+1] = output[[length(output)]][4]
+      df2 = data.frame(hullX, hullY)
+      hullplot = p + geom_point(df2,mapping=aes(x=hullX,y=hullY),color="red") + geom_path(df2,mapping=aes(x=hullX,y=hullY),colour="red")    
+      print(hullplot)
+    }
+    output
+  }else if(class(input)=="numeric"){
+    
+    if(length(input)%%2 != 0){
+      stop("Not all points have length 2")
+    }
+    
+    list_of_points <- list()
+    for(i in 1:(length(input)/2)){
+      list_of_points[i] <- list(c(input[2*i-1],input[2*i]))
+    }
+    
+    output<-jarvis_march(list_of_points)
+    if(plot==TRUE){
+      require(rlang)
+      require(ggplot2)
+      X=c()
+      Y=c()
+      for (i in 1:length(list_of_points)) {
+        X[i] = list_of_points[[i]][1]
+        Y[i] = list_of_points[[i]][2]
+      }
+      df=data.frame(X, Y)
+      p = ggplot(df,aes(x=X,y=Y)) + geom_point()
+      hullX=c()
+      hullY=c()
+      for (i in 1:length(output)) {
+        hullX[i] = output[[i]][1]
+        hullY[i] = output[[i]][2]
+      }
+      hullX[length(output)+1] = output[[length(output)]][3] ; hullY[length(output)+1] = output[[length(output)]][4]
+      df2 = data.frame(hullX, hullY)
+      hullplot = p + geom_point(df2,mapping=aes(x=hullX,y=hullY),color="red") + geom_path(df2,mapping=aes(x=hullX,y=hullY),colour="red")    
+      print(hullplot)
+    }
+    output
+    
+    
+  }else if(class(input)=="data.frame"){
+    
+    list_of_points<-list()
+    for(i in 1:nrow(input)){
+      list_of_points[i] <- list(c(input[i,1],input[i,2]))
+    }
+    
+    output<-jarvis_march(list_of_points)
+    if(plot==TRUE){
+      require(rlang)
+      require(ggplot2)
+      X=c()
+      Y=c()
+      for (i in 1:length(list_of_points)) {
+        X[i] = list_of_points[[i]][1]
+        Y[i] = list_of_points[[i]][2]
+      }
+      df=data.frame(X, Y)
+      p = ggplot(df,aes(x=X,y=Y)) + geom_point()
+      hullX=c()
+      hullY=c()
+      for (i in 1:length(output)) {
+        hullX[i] = output[[i]][1]
+        hullY[i] = output[[i]][2]
+      }
+      hullX[length(output)+1] = output[[length(output)]][3] ; hullY[length(output)+1] = output[[length(output)]][4]
+      df2 = data.frame(hullX, hullY)
+      hullplot = p + geom_point(df2,mapping=aes(x=hullX,y=hullY),color="red") + geom_path(df2,mapping=aes(x=hullX,y=hullY),colour="red")    
+      print(hullplot)
+    }
+    output
+    
+  }else{
+    stop("Input not in the class of a list, numeric vector or data frame")
+  }
+}
